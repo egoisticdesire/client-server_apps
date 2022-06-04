93037715d6d668802027ebcf7812e19465b08811
egoistic_desire <kfkfjqjq@mail.ru>
2022-06-05 03:46:14 +0400

    hw3

---
+++ l3/common/utils.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import json
+from .variables import *
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode(ENCODING)
+        if isinstance(json_response, str):
+            response = json.loads(json_response)
+            if isinstance(response, dict):
+                return response
+            raise ValueError
+        raise ValueError
+    raise ValueError
+
+
+def send_message(sock, message):
+    if not isinstance(message, dict):
+        raise TypeError
+    json_message = json.dumps(message)
+    encoded_message = json_message.encode(ENCODING)
+    sock.send(encoded_message)
